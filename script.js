5014aaeacb92f285b06fc1a879aa3acea96bd730
Mykola Goral <mykolag21@gmail.com>
2017-11-14 19:13:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+var tabs = document.getElementById("tabs").getElementsByTagName("li");
+var work = document.getElementById("work");
+var recreating = document.getElementById("recreating");
+recreating.style.display = "none";
+//var work = document.getElementsByClassName("work");
+
+tabs[0].classList.add("activeTab");
+//document.getElementsByClassName("work").style.backgroundColor = "lightblue";
+
+tabs[0].addEventListener("click", function () {
+    tabs[0].classList.add("activeTab");
+    tabs[1].classList.remove("activeTab");
+
+    work.style.display = "block";
+    recreating.style.display = "none";
+});
+
+tabs[1].addEventListener("click", function () {
+    tabs[1].classList.add("activeTab");
+    tabs[0].classList.remove("activeTab");
+
+    work.style.display = "none";
+    recreating.style.display = "block";
+});
+
+
+
+// make the second tab active for easier manipulation
+//tabs[1].classList.add("activeTab");
+//work.style.display = "none";
+//recreating.style.display = "block";
+
+
+
+
+
+
+
+
+
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
