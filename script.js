aeeae41a5da9dd52c4140da82fca29f0c8fc1a5d
Mykola Goral <mykolag21@gmail.com>
2017-12-12 17:01:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ tabs[1].addEventListener("click", function () {
 
 
 // make the second tab active for easier manipulation
+
 //tabs[1].classList.add("activeTab");
 //work.style.display = "none";
 //recreating.style.display = "block";
@@ -42,13 +43,21 @@ var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('myImg');
+var img2 = document.getElementById('myImg2');
+
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function () {
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
-}
+};
+img2.onclick = function () {
+    var img2 = document.querySelector('.fullImgHidden');
+    modal.style.display = "block";
+    modalImg.src = img2.src;
+    captionText.innerHTML = img2.alt;
+};
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
